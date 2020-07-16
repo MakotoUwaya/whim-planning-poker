0475b049acbf7d452d4cb6037e46066358f57516
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-07-16 14:27:45 +0900

    change vuetify theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,13 +8,14 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
-        accent: "#82B1FF",
+        primary: "#1F76DD",
+        secondary: "#EDC32F",
+        accent: "#04E3EC",
+        neutral: "#113364",
         error: "#FF5252",
         info: "#2196F3",
         success: "#4CAF50",
-        warning: "#FFC107"
+        warning: "#FB8C00"
       }
     }
   },
